bb91806a3872e1b3ddd95d586ce3c0a47adb24c8
Rodrigo Lins <rodrigonascimentolins2@gmail.com>
2021-08-17 21:12:32 -0300

    desafio aula 2
    desafio aula 2

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace desafio_aula_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       string nomeAluno;
+       float matricula, nota1, nota2, media;
+
+       Console.WriteLine("Digite o nome do aluno: ");
+       nomeAluno = Console.ReadLine();
+
+       Console.WriteLine("Digite a matrícula do aluno: ");
+       matricula = float.Parse(Console.ReadLine());
+
+       Console.WriteLine("Digite a nota do 1º Bimestre: ");
+       nota1 = float.Parse(Console.ReadLine());
+
+       Console.WriteLine("Digite a nota do 2º Bimestre: ");
+       nota2 = float.Parse(Console.ReadLine());
+
+       media = (nota1 + nota2)/2;
+
+       if ( media >= 6)
+       {
+           Console.Write($" {nomeAluno}. Aprovado com média {media}");
+       }
+       else 
+       {
+           Console.Write($" {nomeAluno}. Reprovado com média {media}");
+       }
+        }
+    }
+}
